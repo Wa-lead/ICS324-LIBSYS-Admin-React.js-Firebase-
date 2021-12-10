ead56c899a86adaa43a13694c04a2b8a5b1109ba
Wa-lead <waleedaaofficial@gmail.com>
2021-12-10 23:17:24 +0300

    Navbar Modification

--- src/comp/SideBar.js
+++ src/comp/SideBar.js
@@ -18,7 +18,7 @@ const SideBar = ({ setView }) => {
 
 
         <div className="Navbar">
-            <Typography variant="h5" sx={{ m: 2 }}> KFUPM LIBSYS</Typography>
+            <Typography variant="h4" sx={{ m: 2 }}> KFUPM LIBSYS</Typography>
 
             <Stack direction="row" spacing={1} sx={{ m: 2 }}>
                 <Chip label="Books" onClick={() => handleClick("Books")} />
